4c2a6c7d3884fe6d01ec88f4b5de1c6e2fe84abb
prajy <prajy@SAN76638>
2021-02-09 12:11:32 +1030

    Firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ResumeComponent } from './component/resume/resume.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SectionComponent } from './component/section/section.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ResumeComponent } from "./component/resume/resume.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { SectionComponent } from "./component/section/section.component";
+
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent, ResumeComponent, SectionComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    AngularFireModule.initializeApp(environment.firebase),
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
